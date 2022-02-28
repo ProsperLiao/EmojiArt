6f2b5dfa51639210b4d6d247100a0b2b03b4f2f9
Hongxing Liao <hx.liao@gmail.com>
2022-02-28 20:34:36 +0800

    Initial Commit

---
+++ Shared/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  Shared
+//
+//  Created by Hongxing Liao on 2022/2/28.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
